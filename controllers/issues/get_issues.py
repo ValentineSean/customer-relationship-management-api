7ba789b0d08b00f350237a3106621953e801645b
Valentine Chanengeta <seanchanengeta@gmail.com>
2022-08-28 16:23:28 +0200

    filtering issues implemented

--- controllers/issues/get_issues.py
+++ controllers/issues/get_issues.py
@@ -8,14 +8,41 @@ get_issues_blueprint = Blueprint("get_issues_blueprint", __name__)
 @get_issues_blueprint.route("/get-issues")
 def get_issues():
     customer_id = request.args.get("customer_id")
+    issue_status = request.args.get("issue_status")
     issues = []
 
     try:
         if customer_id:
-            issues = Issue.find((Issue.sender==customer_id)&(Issue.record_status=="ALIVE")).all()
+            if issue_status == "OPEN":
+                issues = Issue.find((Issue.sender==customer_id)&(Issue.issue_status=="OPEN")&(Issue.record_status=="ALIVE")).all()
+
+            elif issue_status == "CLOSED":
+                issues = Issue.find((Issue.sender==customer_id)&(Issue.issue_status=="CLOSED")&(Issue.record_status=="ALIVE")).all()
+
+            elif issue_status == "PENDING":
+                issues = Issue.find((Issue.sender==customer_id)&(Issue.issue_status=="PENDING")&(Issue.record_status=="ALIVE")).all()
+
+            elif issue_status == "PROGRESS":
+                issues = Issue.find((Issue.sender==customer_id)&(Issue.issue_status=="PROGRESS")&(Issue.record_status=="ALIVE")).all()
+
+            else:
+                issues = Issue.find((Issue.sender==customer_id)&(Issue.record_status=="ALIVE")).all()
 
         else:
-            issues = Issue.find(Issue.record_status=="ALIVE").all()
+            if issue_status == "OPEN":
+                issues = Issue.find((Issue.issue_status=="OPEN")&(Issue.record_status=="ALIVE")).all()
+
+            elif issue_status == "CLOSED":
+                issues = Issue.find((Issue.issue_status=="CLOSED")&(Issue.record_status=="ALIVE")).all()
+
+            elif issue_status == "PENDING":
+                issues = Issue.find((Issue.issue_status=="PENDING")&(Issue.record_status=="ALIVE")).all()
+
+            elif issue_status == "PROGRESS":
+                issues = Issue.find((Issue.issue_status=="PROGRESS")&(Issue.record_status=="ALIVE")).all()
+
+            else:
+                issues = Issue.find(Issue.record_status=="ALIVE").all()
 
         issues_list = []
 
